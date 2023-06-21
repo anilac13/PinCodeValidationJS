a63ab955a520f35d4bb9278a0c9ec12274a0f1b0
anilac13 <anil4u2496@gmail.com>
2023-06-21 15:28:22 +0530

    [Anil] Add:Check entered Pin code is valid or not

--- validation.js
+++ validation.js
@@ -1,10 +1,10 @@
 let pincode = RegExp('^[0-9]{6}$');
-let input = "A400048";
+let input = "400048B";
 
 try{
     if (pincode.test(input))
-    console.log("Valid");
-    else throw "Invalid"
+    console.log("Pin Code is Valid");
+    else throw "Pin Code is Invalid"
 }
 catch(e){
     console.log(e);
